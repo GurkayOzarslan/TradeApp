1fbf16efd23a4eaf3413fcf7ecc5dffee1bb3966
Gürkay Özarslan <gurkay10@outlook.com>
2025-05-07 22:42:15 +0300

    fx

--- TradeAppEntities/Portfolio.cs
+++ TradeAppEntities/Portfolio.cs
@@ -3,7 +3,7 @@
     public class Portfolio
     {
         public int Id { get; private set; }
-        public string UserId { get; private set; }
+        public int UserId { get; private set; }
         public string AssetName { get; private set; }
         public decimal Amount { get; private set; }
         public decimal PurchasePrice { get; private set; }
